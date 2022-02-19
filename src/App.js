6cbac2d8682f497618454cb99a1aba63ddf94904
dshibaev <dmitry.shibaev@gmail.com>
2022-02-19 13:10:17 +0000

    added AssetName String and improved commentary

--- src/App.js
+++ src/App.js
@@ -91,9 +91,26 @@ export default class App extends React.Component
 
         }
 
+        /**
+         * When the wallet is connect it returns the connector which is
+         * written to this API variable and all the other operations
+         * run using this API object
+         */
         this.API = undefined;
 
-
+        /**
+         * Protocol parameters
+         * @type {{
+         * keyDeposit: string,
+         * coinsPerUtxoWord: string,
+         * minUtxo: string,
+         * poolDeposit: string,
+         * maxTxSize: number,
+         * priceMem: number,
+         * maxValSize: number,
+         * linearFee: {minFeeB: string, minFeeA: string}, priceStep: number
+         * }}
+         */
         this.protocolParams = {
             linearFee: {
                 minFeeA: "44",
@@ -112,8 +129,17 @@ export default class App extends React.Component
 
     }
 
+    /**
+     * Handles the tab selection on the user form
+     * @param tabId
+     */
     handleTabId = (tabId) => this.setState({selectedTabId: tabId})
 
+    /**
+     * Handles the radio buttons on the form that
+     * let the user choose which wallet to work with
+     * @param obj
+     */
     handleWalletSelect = (obj) => {
         const whichWalletSelected = obj.target.value
         this.setState({whichWalletSelected},
@@ -121,6 +147,12 @@ export default class App extends React.Component
                 this.refreshData()
             })
     }
+
+    /**
+     * Checks if the wallet is running in the browser
+     * Does this for Nami, CCvault and Flint wallets
+     * @returns {boolean}
+     */
 
     checkIfWalletFound = () => {
         let walletFound = false;
@@ -138,6 +170,11 @@ export default class App extends React.Component
         return walletFound;
     }
 
+    /**
+     * Checks if a connection has been established with
+     * the wallet
+     * @returns {Promise<boolean>}
+     */
     checkIfWalletEnabled = async () => {
 
         let walletIsEnabled = false;
@@ -161,6 +198,14 @@ export default class App extends React.Component
         return walletIsEnabled
     }
 
+    /**
+     * Enables the wallet that was chosen by the user
+     * When this executes the user should get a window pop-up
+     * from the wallet asking to approve the connection
+     * of this app to the wallet
+     * @returns {Promise<void>}
+     */
+
     enableWallet = async () => {
         try {
 
@@ -181,6 +226,11 @@ export default class App extends React.Component
         }
     }
 
+    /**
+     * Get the API version used by the wallets
+     * writes the value to state
+     * @returns {*}
+     */
     getAPIVersion = () => {
 
         let walletAPIVersion;
@@ -198,7 +248,11 @@ export default class App extends React.Component
         return walletAPIVersion;
     }
 
-
+    /**
+     * Get the name of the wallet (nami, ccvault, flint)
+     * and store the name in the state
+     * @returns {*}
+     */
 
     getWalletName = () => {
 
@@ -217,6 +271,13 @@ export default class App extends React.Component
         return walletName;
     }
 
+    /**
+     * Gets the Network ID to which the wallet is connected
+     * 0 = testnet
+     * 1 = mainnet
+     * Then writes either 0 or 1 to state
+     * @returns {Promise<void>}
+     */
     getNetworkId = async () => {
         try {
             const networkId = await this.API.getNetworkId();
@@ -226,6 +287,12 @@ export default class App extends React.Component
             console.log(err)
         }
     }
+
+    /**
+     * Gets the UTXOs from the user's wallet and then
+     * stores in an object in the state
+     * @returns {Promise<void>}
+     */
 
     getUtxos = async () => {
 
@@ -261,11 +328,12 @@ export default class App extends React.Component
 
                         for (let j = 0; j < K; j++) {
                             const assetName = assetNames.get(j);
+                            const assetNameString = Buffer.from(assetName.name(),"utf8").toString();
                             const assetNameHex = Buffer.from(assetName.name(),"utf8").toString("hex")
                             // console.log(`Asset Name: ${assetNameHex}`)
                             const multiassetAmt = multiasset.get_asset(policyId, assetName)
-                            // console.log(`Asset Amount: ${multiassetAmt.to_str()}`)
-                            multiAssetStr += `+ ${multiassetAmt.to_str()} + ${policyIdHex}.${assetNameHex}`
+                            multiAssetStr += `+ ${multiassetAmt.to_str()} + ${policyIdHex}.${assetNameHex} (${assetNameString})`
+                            // console.log(assetNameString)
                         }
                     }
                 }
@@ -288,6 +356,15 @@ export default class App extends React.Component
         }
     }
 
+    /**
+     * The collateral is need for working with Plutus Scripts
+     * Essentially you need to provide collateral to pay for fees if the
+     * script execution fails after the script has been validated...
+     * this should be an uncommon occurrence and would suggest the smart contract
+     * would have been incorrectly written.
+     * The amount of collateral to use is set in the wallet
+     * @returns {Promise<void>}
+     */
     getCollateral = async () => {
 
         let CollatUtxos = [];
@@ -315,6 +392,12 @@ export default class App extends React.Component
 
     }
 
+    /**
+     * Gets the current balance of in Lovelace in the user's wallet
+     * This doesnt resturn the amounts of all other Tokens
+     * For other tokens you need to look into the full UTXO list
+     * @returns {Promise<void>}
+     */
     getBalance = async () => {
         try {
             const balanceCBORHex = await this.API.getBalance();
@@ -327,6 +410,11 @@ export default class App extends React.Component
         }
     }
 
+    /**
+     * Get the address from the wallet into which any spare UTXO should be sent
+     * as change when building transactions.
+     * @returns {Promise<void>}
+     */
     getChangeAddress = async () => {
         try {
             const raw = await this.API.getChangeAddress();
@@ -337,6 +425,10 @@ export default class App extends React.Component
         }
     }
 
+    /**
+     * This is the Staking address into which rewards from staking get paid into
+     * @returns {Promise<void>}
+     */
     getRewardAddresses = async () => {
 
         try {
@@ -351,6 +443,10 @@ export default class App extends React.Component
         }
     }
 
+    /**
+     * Gets previsouly used addresses
+     * @returns {Promise<void>}
+     */
     getUsedAddresses = async () => {
 
         try {
@@ -365,6 +461,10 @@ export default class App extends React.Component
         }
     }
 
+    /**
+     * Refresh all the data from the user's wallet
+     * @returns {Promise<void>}
+     */
     refreshData = async () => {
         try{
             const walletFound = this.checkIfWalletFound();
@@ -384,7 +484,12 @@ export default class App extends React.Component
         }
     }
 
-
+    /**
+     * Every transaction starts with initializing the
+     * TransactionBuilder and setting the protocol parameters
+     * This is boilerplate
+     * @returns {Promise<TransactionBuilder>}
+     */
     initTransactionBuilder = async () => {
 
         const txBuilder = TransactionBuilder.new(
@@ -402,6 +507,10 @@ export default class App extends React.Component
         return txBuilder
     }
 
+    /**
+     * Builds an object with all the UTXOs from the user's wallet
+     * @returns {Promise<TransactionUnspentOutputs>}
+     */
     getTxUnspentOutputs = async () => {
         let txOutputs = TransactionUnspentOutputs.new()
         for (const utxo of this.state.Utxos) {
@@ -410,7 +519,17 @@ export default class App extends React.Component
         return txOutputs
     }
 
-
+    /**
+     * The transaction is build in 3 stages:
+     * 1 - initialize the Transaction Builder
+     * 2 - Add inputs and outputs
+     * 3 - Calculate the fee and how much change needs to be given
+     * 4 - Build the transaction body
+     * 5 - Sign it (at this point the user will be prompted for
+     * a password in his wallet)
+     * 6 - Send the transaction
+     * @returns {Promise<void>}
+     */
     buildSendADATransaction = async () => {
 
         const txBuilder = await this.initTransactionBuilder();
